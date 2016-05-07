c5ea17738a43b38ea3e623aeb7d555ec17988e1c
ysden123 <ysden123@gmail.com>
2016-05-07 22:03:24 +0300

    FileInfo: exists

--- test/fileinfo_test.js
+++ test/fileinfo_test.js
@@ -1,13 +1,35 @@
 // Unit test for class FileInfo
 'use strict';
-const should = require('chai').should()
-const os = require('os')
-const fs = require('fs')
-const path = require('path')
+const should = require('chai').should(),
+    os = require('os'),
+    fs = require('fs'),
+    path = require('path'),
+    FileInfo = require('..').FileInfo;
 
-// var fi = new require('../lib/fileinfo')('test.ext');
-var FileInfo = require('..').FileInfo
-var fi = new FileInfo('test.txt');
 
-console.log(fi.fileName());
-console.log(fi.exists());
+describe('FileInfo', function () {
+    let testFile = path.join(os.tmpdir(), 'checker_test.txt');
+
+    before(function () {
+        fs.writeFileSync(testFile, 'abc');
+    })
+
+    after(function () {
+        try {
+            fs.unlink(testFile)
+        } catch (e) { }
+    })
+
+    describe('#exists', function () {
+        it('Should check directory', function () {
+            let fi = new FileInfo(testFile);
+            fi.exists().should.be.equals(true);
+            fi = new FileInfo('XYZbred3322');
+            fi.exists().should.be.equals(false);
+        })
+        it('Should check file', function () {
+            let fi = new FileInfo(testFile);
+            fi.exists().should.be.equals(true);
+        })
+    })
+})
