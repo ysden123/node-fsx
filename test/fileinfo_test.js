dd27d6acb640c54b91e7fbbf2bc1e2eb366fa9a1
ysden123 <ysden123@gmail.com>
2016-05-07 21:25:18 +0300

    tests

--- test/fileinfo_test.js
+++ test/fileinfo_test.js
@@ -1,7 +1,13 @@
+// Unit test for class FileInfo
 'use strict';
+const should = require('chai').should()
+const os = require('os')
+const fs = require('fs')
+const path = require('path')
+
 // var fi = new require('../lib/fileinfo')('test.ext');
-var FileInfo = require('../lib/fileinfo')
+var FileInfo = require('..').FileInfo
 var fi = new FileInfo('test.txt');
 
 console.log(fi.fileName());
-// console.log(fi.exists());
+console.log(fi.exists());
